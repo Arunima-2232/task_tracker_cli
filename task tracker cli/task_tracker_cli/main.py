e015342ef8da40e6a8729b075976ee942f87c166
Arunima-2232 <145280505+Arunima-2232@users.noreply.github.com>
2026-01-10 09:06:14 +0530

    Update main.py
    added more features

--- task tracker cli/task_tracker_cli/main.py
+++ task tracker cli/task_tracker_cli/main.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import os
 
+from datetime import datetime
+
 data_file = os.path.join(os.path.dirname(__file__), "data.json")
 
 def load_data():
@@ -16,7 +18,7 @@ def save_data(data):
 
 def main():
     parser=argparse.ArgumentParser(description="A simple task tracking CLI application")
-    parser.add_argument("operation",choices=["add","delete","show"],help="Operation to perform")
+    parser.add_argument("operation",choices=["add","delete","update","mark-done","show","show-done","show-not-done"],help="Operation to perform")
     parser.add_argument("task",type=str,nargs="*",help="task to be added or deleted")
     args=parser.parse_args()
     data=load_data()
@@ -26,7 +28,7 @@ def main():
     try:
         task=" ".join(args.task)
         if args.operation=="add":
-            data[str(next_id)]=task
+            data[str(next_id)]=[task,"Not done",datetime.now().strftime('%d/%m/%Y, %H:%M'),"--"]
             print(f"{task} added to task-list! ID: {next_id}")
         elif args.operation=="delete":
             task_id=None
@@ -35,8 +37,32 @@ def main():
                     task_id=key
                     break
             if task_id:
-                print(f"{data[task_id]} deleted from task-list!")
+                print(f"{data[task_id][0]} deleted from task-list!")
                 del data[task_id]
+            else:
+                print("Task doesn't exist")
+        elif args.operation=="update":
+            task_id=None
+            for key,value in data.items():
+                if key==args.task[0]:
+                    task_id=key
+                    data[task_id][0]=args.task[1]
+                    data[task_id][3]=datetime.now().strftime('%d/%m/%Y, %H:%M')
+                    break
+            if task_id:
+                print(f"Task {task_id} updated to {data[task_id][0]}!")
+            else:
+                print("Task doesn't exist")
+        elif args.operation=="mark-done":
+            task_id=None
+            for key,value in data.items():
+                if key==task:
+                    task_id=task
+                    data[task_id][1]="Done!"
+                    data[task_id][3]=datetime.now().strftime('%d/%m/%Y, %H:%M')
+                    break
+            if task_id:
+                print("Task marked as done!")
             else:
                 print("Task doesn't exist")
         elif args.operation=="show":
@@ -45,7 +71,31 @@ def main():
             else:
                 print("To-Do")
                 for key,value in data.items():
-                    print(key+".",value)
+                    print(key+".",value[0],"-",value[1],"- created at",value[2],"- updated at", value[3])
+        elif args.operation=="show-done":
+            if not data:
+                print("No tasks")
+            else:
+                task_id=None
+                print("Tasks marked done:")
+                for key,value in data.items():
+                    if(value[1]=="Done!"):
+                        task_id=key
+                        print(key+".",value[0])
+                if(task_id==None):
+                    print("None")
+        elif args.operation=="show-not-done":
+            if not data:
+                print("No tasks")
+            else:
+                task_id=None
+                print("Tasks marked not done:")
+                for key,value in data.items():
+                    if(value[1]=="Not done"):
+                        task_id=key
+                        print(key+".",value[0])
+                if(task_id==None):
+                    print("None")
         save_data(data)
                 
     except ValueError as e:
